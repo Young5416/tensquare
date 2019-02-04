715e11b96dc15934e6b8cfe85a7512494c49e66c
Young <yl18755144431@gmail.com>
2019-02-04 20:38:43 +0800

    tensquare_base启动类及yml配置文件,分布式id生成器

---
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,25 @@
+package com.tensquare.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * @description: tensquare_base 启动类
+ * @author: Young
+ * @create: 2019-02-04 20:34
+ **/
+
+@SpringBootApplication
+
+public class BaseApplication {
+  public static void main(String[] args) {
+      SpringApplication.run(BaseApplication.class);
+  }
+
+  @Bean
+  public IdWorker idWorker(){
+      return new IdWorker(1,1);
+  }
+}
