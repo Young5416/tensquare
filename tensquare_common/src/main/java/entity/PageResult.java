6ac67d72259c8e4ca4aaa84b1a7b27e5c492288f
Young <yl18755144431@gmail.com>
2019-02-06 16:34:56 +0800

    lombok引入 logback配置

--- tensquare_common/src/main/java/entity/PageResult.java
+++ tensquare_common/src/main/java/entity/PageResult.java
@@ -1,38 +1,20 @@
 package entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 /**
  * @description: 返回分页实体类
  * @author: Young
  * @create: 2019-02-04 19:53
- **/
-
-public class PageResult <T>{
-    private long total;
-    private List<T> rows;
-
-    public PageResult() {
-    }
-
-    public PageResult(long total, List<T> rows) {
-        this.total = total;
-        this.rows = rows;
-    }
-
-    public long getTotal() {
-        return total;
-    }
-
-    public void setTotal(long total) {
-        this.total = total;
-    }
-
-    public List<T> getRows() {
-        return rows;
-    }
-
-    public void setRows(List<T> rows) {
-        this.rows = rows;
-    }
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResult<T> {
+  private long total;
+  private List<T> rows;
 }
