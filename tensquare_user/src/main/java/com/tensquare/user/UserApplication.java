f2c4f46b2564ba5ad11bd8fb39e9b60e27bebc74
Young5416 <1101744096@qq.com>
2019-02-17 21:54:09 +0800

    eureka模块 客户端和注册端配置

--- tensquare_user/src/main/java/com/tensquare/user/UserApplication.java
+++ tensquare_user/src/main/java/com/tensquare/user/UserApplication.java
@@ -2,6 +2,7 @@ package com.tensquare.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import util.IdWorker;
@@ -13,6 +14,7 @@ import util.JwtUtil;
  * @create: 2019-02-09 20:15
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class UserApplication {
   public static void main(String[] args) {
     SpringApplication.run(UserApplication.class);
